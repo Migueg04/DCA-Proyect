4aabf6e93d738fbdfba0920d037d4db1b07676b0
SALOMEDIAZ <diazcardonasalome28@gmail.com>
2025-04-09 17:55:46 -0500

    last updates feat searchbar

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 export default {
-    entry: './src/components/Searchbar.ts',
+    entry: './src/index.ts',
     module: {
         rules: [
             {
