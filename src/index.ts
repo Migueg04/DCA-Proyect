1dee9438fc34e74277d4cfded1883ccae570cbcb
Migueg04 <miguegiraldollamosa@gmail.com>
2025-04-11 14:56:13 -0500

    fix(index.html changes)

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,1 @@
 
-
-import { createDropCard } from "./components/dropCard";
-import drops from "./data/drops.json";
-
-const container = document.getElementById("drops-container");
-if (!container) throw new Error("Contenedor de drops no encontrado");
-
-drops.forEach((drop:any)  => {
-  const card = createDropCard(drop);
-  container.appendChild(card);
-});
-
